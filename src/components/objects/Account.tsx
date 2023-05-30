03a25b61af7ae4c930ad5a4fbe465be02ad1c855
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-05-30 10:30:29 +0700

    More attributes and methods in Account.tsx

--- src/components/objects/Account.tsx
+++ src/components/objects/Account.tsx
@@ -1,9 +1,27 @@
 import {AccountRole} from "./AccountRole";
 
 export class Account {
+    private name: string;
     private role: AccountRole;
 
-    constructor(role: AccountRole) {
+    constructor(name: string, role: AccountRole) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getRole(): AccountRole {
+        return this.role;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public setRole(role: AccountRole): void {
         this.role = role;
     }
 }
