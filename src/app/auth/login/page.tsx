0295967a396ae7bb6d246f487d7ce20ede7958ed
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-05-30 09:01:03 +0700

    Added login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,52 @@
+export default function Page() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+                <h2 className="text-2xl font-bold text-gray-800 mb-4">Login</h2>
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="email"
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <div className="flex gap-3">
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit">
+                                Sign In
+                            </button>
+                            <button
+                                className="hover:text-gray-400 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit">
+                                Register
+                            </button>
+                        </div>
+                        <a
+                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                            href="/forgot-password">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
