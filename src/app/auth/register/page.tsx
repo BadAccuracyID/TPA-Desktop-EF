65441d3b4e93806013374d954841b377d35cca6c
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-05-30 09:28:02 +0700

    Added login and register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
             <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
                 <div className="flex gap-2 items-center mb-4 text-blue-700">
                     <LockClosedIcon className="h-6 w-auto"/>
-                    <h2 className="text-2xl font-bold text-gray-800">Login</h2>
+                    <h2 className="text-2xl font-bold text-gray-800">Register</h2>
                 </div>
                 <form>
                     <div className="mb-4">
@@ -21,36 +21,42 @@ export default function Page() {
                             placeholder="Enter your email"
                         />
                     </div>
-                    <div className="mb-6">
+                    <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             Password
                         </label>
                         <input
-                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight focus:outline-none focus:shadow-outline"
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="password"
                             type="password"
                             placeholder="Enter your password"
                         />
-                        <Link
-                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 mb-1"
-                            href="/forgot-password">
-                            Forgot Password?
-                        </Link>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Confirm Password
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                        />
                     </div>
                     <div className="flex justify-between">
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                             type="submit">
-                            Sign In
+                            Register
                         </button>
                         <Link
-                            href="/auth/register"
+                            href="/auth/login"
                             className="text-sm py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             <text>
-                                Don&apos;t have an account?&nbsp;
+                                Already have an account?&nbsp;
                             </text>
                             <text className="text-blue-700 font-bold">
-                                Register here.
+                                Login here.
                             </text>
                         </Link>
                     </div>
