8b655415249559660562a2f12aef8eb498bff3ba
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-06-02 23:34:02 +0700

    Added action buttons to StaffCard.tsx

--- src/components/card/staff/StaffCard.tsx
+++ src/components/card/staff/StaffCard.tsx
@@ -1,7 +1,8 @@
 import {AccountRole} from "@/components/objects/AccountRole";
 import {formatDate, toDateFromSeconds} from "@/utils/AccountUtils";
 import {Account} from "@/components/objects/Account";
-import {UserCircleIcon, WrenchIcon, XMarkIcon} from "@heroicons/react/20/solid";
+import {CheckCircleIcon, UserCircleIcon, WrenchIcon, XMarkIcon} from "@heroicons/react/20/solid";
+import React, {useState} from "react";
 
 export const StaffCard = ({id, name, email, role, verified, onClick}: {
     id: string;
@@ -45,14 +46,27 @@ export const StaffSettingsModel = ({account, onClose}: {
 }) => {
     const createdAt: any | null = account.getCreatedAt();
     const verifiedAt: any | null = account.getVerifiedAt();
+    const [selectedRole, setSelectedRole] = useState(account.getRole().toString());
 
     const handleClose = () => {
         onClose();
     };
 
+    const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedRole(event.target.value);
+    };
+
+    const handleToggleVerification = () => {
+    };
+
+    const handleDeleteAccount = () => {
+    };
+
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
+            <div className="absolute inset-0 bg-gray-900 opacity-50"
+                 onClick={handleClose}/>
+
             <div className="relative bg-white text-black shadow-lg rounded-md p-4">
                 <div className="flex flex-row items-center gap-1.5">
                     <UserCircleIcon className="w-7"/>
@@ -87,6 +101,48 @@ export const StaffSettingsModel = ({account, onClose}: {
                         <span className="font-semibold">Verified At:</span>{" "}
                         {verifiedAt ? formatDate(toDateFromSeconds(verifiedAt)) : "Never"}
                     </p>
+                </div>
+
+                {/* Role selection */}
+                <div className="pt-4 flex flex-col gap-2">
+                    <label htmlFor="roleSelect" className="font-semibold">
+                        Change Role:
+                    </label>
+                    <div className="flex flex-row gap-2">
+                        <select
+                            id="roleSelect"
+                            value={selectedRole}
+                            onChange={handleRoleChange}
+                            className="border border-gray-300 rounded-md p-2 w-full">
+                            {
+                                Object.values(AccountRole)
+                                    .filter((value) => typeof value === 'string')
+                                    .map((role) => {
+                                        return (
+                                            <option key={role.toString()} value={role.toString()}>
+                                                {role.toString()}
+                                            </option>
+                                        )
+                                    })
+                            }
+                        </select>
+                        <button className="">
+                            <CheckCircleIcon
+                                className="w-9 h-auto bg-green-500 hover:bg-green-400 p-1 text-white rounded-md cursor-pointer"/>
+                        </button>
+                    </div>
+                </div>
+
+                {/* Buttons */}
+                <div className="pt-4 flex flex-row justify-center gap-4">
+                    <button>
+                        <span
+                            className="text-white bg-blue-600 hover:bg-blue-500 p-2 rounded-md">Toggle Verification</span>
+                    </button>
+                    <button>
+                        <span
+                            className="text-white bg-red-600 hover:bg-red-500 p-2 rounded-md">Delete Account</span>
+                    </button>
                 </div>
             </div>
         </div>
